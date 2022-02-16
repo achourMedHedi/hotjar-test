ee1414f7c77e9cc4612253c22130c74167aebd20
achour <achourmedhedi@gmail.com>
2022-02-16 12:27:14 +0100

    9

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-// import { useEffect } from 'react';
-// import useHotjar from 'react-use-hotjar';
-// const myCustomLogger = console.info;
+import { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
+import useHotjar from 'react-use-hotjar';
+const myCustomLogger = console.info;
 
 function App() {
-  // const { initHotjar } = useHotjar();
-  // const { identifyHotjar } = useHotjar();
+  useEffect(() => {
+    hotjar.initialize(2824598, 6);
+    hotjar.identify("we are testing here email ", {userId: "123456789 ", extra: "extra 12"});
+  }, [])
+  const { initHotjar } = useHotjar();
+  const { identifyHotjar } = useHotjar();
 
-  // useEffect(() => {
-  //   initHotjar(2824598, 6, false, myCustomLogger);
-  // }, [initHotjar]);
+  useEffect(() => {
+    initHotjar(2824598, 6, false, myCustomLogger);
+  }, [initHotjar]);
   
-  // useEffect(() => {
-  //   identifyHotjar(
-  //     "user id testing",
-  //     JSON.stringify({test: "testing", notNow: "not ow"}),
-  //     myCustomLogger
-  //   );
-  // }, [identifyHotjar]);
+  useEffect(() => {
+    identifyHotjar(
+      "user id testing",
+      JSON.stringify({test: "testing", notNow: "not ow"}),
+      myCustomLogger
+    );
+  }, [identifyHotjar]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 222 <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
