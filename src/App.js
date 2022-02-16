db9e042a68646996c2b71c9d1266e798f5f7170f
achour <achourmedhedi@gmail.com>
2022-02-16 11:56:28 +0100

    fifth

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import { hotjar } from 'react-hotjar';
+import useHotjar from 'react-use-hotjar';
+const myCustomLogger = console.info;
 
 function App() {
+  // useEffect(() => {
+  //   hotjar.initialize(2824598, 6);
+  //   hotjar.identify({userId: "userId testing"}, {userId: "test id", extra: "extra info"});
+  // }, [])
+  const { initHotjar } = useHotjar();
+  const { identifyHotjar } = useHotjar();
+
   useEffect(() => {
-    hotjar.initialize(2824598, 6);
-    hotjar.identify({userId: "userId testing"}, {userId: "test id", extra: "extra info"});
-  }, [])
+    initHotjar(2824598, 6, false, myCustomLogger);
+  }, [initHotjar]);
+  
+  useEffect(() => {
+    identifyHotjar(
+      "user id testing",
+      JSON.stringify({test: "testing", notNow: "not ow"}),
+      myCustomLogger
+    );
+  }, [identifyHotjar]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit 222 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
